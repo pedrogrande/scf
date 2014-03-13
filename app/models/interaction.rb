f8bf86e22910687dc5265233e6e91a3473fcc526
Pete Argent <pete@thecoderfactory.com>
2014-03-13 15:48:41 +1100

    graph

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,5 +2,7 @@ class Interaction < ActiveRecord::Base
   belongs_to :user
   belongs_to :event_registration
   belongs_to :enquiry
-  belongs_to :
+  belongs_to :testimonial
+  belongs_to :consultation
+  
 end
